5aa1b18e2d273b3f5cdbfffda007f472e8ec61c7
yrlovejava <151824159+yrlovejava@users.noreply.github.com>
2025-06-24 19:13:49 +0800

    init project

---
+++ squirrel-scheduler-admin/src/main/java/com/squirrel/admin/SquirrelSchedulerAdminApplication.java
@@ -0,0 +1,15 @@
+package com.squirrel.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main class for the SquirrelSchedulerAdminApplication.
+ */
+@SpringBootApplication
+public class SquirrelSchedulerAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquirrelSchedulerAdminApplication.class, args);
+    }
+}
