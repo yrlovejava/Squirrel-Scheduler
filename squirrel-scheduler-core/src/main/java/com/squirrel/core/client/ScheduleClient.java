9133ab98ebe00e28b126f1ca80324935c8dd32df
yuanrui.0 <yuanrui.0@bytedance.com>
2025-07-19 13:52:17 +0800

    增加netty客户端

---
+++ squirrel-scheduler-core/src/main/java/com/squirrel/core/client/ScheduleClient.java
@@ -0,0 +1,52 @@
+package com.squirrel.core.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+/**
+ * ScheduleClient
+ * a netty client for schedule to receive task from admin
+ * @author xiaobai
+ * @version 1.0.0
+ * @since 2025/7/19 13:44
+ */
+public class ScheduleClient {
+    private final String host;
+    private final int port;
+
+    public ScheduleClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException{
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new StringEncoder());
+//                            ch.pipeline().addLast(new AdminClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect(host, port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
